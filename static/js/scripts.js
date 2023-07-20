7ca2d28ef81a6d374473afb53321d8ea278ac765
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2023-07-20 13:28:40 +0300

    fix selection when tamam Retrieved from db

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,13 @@
-  function ChangeFar3y(id){
+function ChangeFar3y(id){
+    flag = false
     var select = document.getElementById('tamam_asasy_'+id).value
+    console.log("Here");
     $.ajax({
       url:'get_far3y/'+select+"/",
       type:'GET',
       success: function(data){
         let far3y_select = document.getElementById("tamam_far3y_"+id)
+        far3y_select.innerHTML = '<option value=""></option>';
         data.tamam_far3y.forEach(t => {
           var option = document.createElement("option")
           option.innerText = t.name;
@@ -17,3 +20,4 @@
       }
     })
   }
+
